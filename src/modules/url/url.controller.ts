9ed657f7e99cf6f85032622ccbd7f29e21338939
Fredaobob <lokgamer01@gmail.com>
2024-10-17 14:20:21 -0300

    chore: enabled cors

--- src/modules/url/url.controller.ts
+++ src/modules/url/url.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Res } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiParam, ApiBody } from '@nestjs/swagger';
 import { UrlService } from './url.service';
 import { CreateUrlDto } from './dto/create-url.dto';
+import { Response } from 'express';
 
 @ApiTags('url')
 @Controller('url')
@@ -18,7 +19,12 @@ export class UrlController {
   @ApiOperation({ summary: 'Redirect to the original URL' })
   @ApiParam({ name: 'code', description: 'The short URL code' })
   @Get(':code')
-  async redirect(@Param('code') code: string): Promise<string> {
-    return this.urlService.redirect(code);
+  async redirect(@Param('code') code: string, @Res() res: Response) {
+    const longUrl = await this.urlService.redirect(code);
+    if (longUrl) {
+      res.redirect(longUrl);
+    } else {
+      res.status(404).send('URL not found');
+    }
   }
 }
