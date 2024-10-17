b5c849a282900d971e93aba393671987cb57413a
Fredaobob <lokgamer01@gmail.com>
2024-10-17 13:13:47 -0300

    feat: created the url service

---
+++ src/modules/url/url.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Url } from './entities/url.entity';
+import { CreateUrlDto } from './dto/create-url.dto';
+
+@Injectable()
+export class UrlService {
+  constructor(@InjectRepository(Url) private urlRepository: Repository<Url>) {}
+
+  async shortenUrl(createUrlDto: CreateUrlDto): Promise<string> {
+    const { longUrl } = createUrlDto;
+    const urlCode = this.generateCode();
+    const shortUrl = `http://localhost:3000/url/${urlCode}`;
+
+    const url = this.urlRepository.create({
+      longUrl,
+      shortUrl,
+      urlCode,
+    });
+
+    await this.urlRepository.save(url);
+    return shortUrl;
+  }
+
+  private generateCode(): string {
+    return Math.random().toString(36).substring(2, 8);
+  }
+
+  async redirect(code: string): Promise<string> {
+    const url = await this.urlRepository.findOne({ where: { urlCode: code } });
+    if (url) {
+      url.visits++;
+      this.urlRepository.save(url);
+      return url.longUrl;
+    } else {
+      throw new Error('URL not found');
+    }
+  }
+}
